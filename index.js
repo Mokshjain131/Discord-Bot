55c952876948efcb17d1c393eb39f46308ed5006
Mokshjain131 <mokshjain131@gmail.com>
2025-03-21 11:11:50 +0530

    chuck norris update

--- index.js
+++ index.js
@@ -10,28 +10,37 @@ const client = new Client({
     ],
 });
 
-const Welcome_Channel_ID = "1242883438214647841";
-const Exec_Role_ID = "1242885356886691871";
+async function generate() {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await response.json();
+    return data.value;
+}
+
+async function chuck() {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await response.json();
+    return data.value;
+}
 
 client.once(Events.ClientReady, () => {
     console.log('Bot is ready!');
 });
 
-client.on(Events.GuildMemberAdd, async (member) => {
-    try {
-        const guild = member.guild;
-        const channel = guild.channels.cache.get(Welcome_Channel_ID);
-        const role = guild.roles.cache.get(Exec_Role_ID);
+// client.on(Events.GuildMemberAdd, async (member) => {
+//     try {
+//         const guild = member.guild;
+//         const channel = guild.channels.cache.get(Welcome_Channel_ID);
+//         const role = guild.roles.cache.get(Exec_Role_ID);
 
-        await member.roles.add(role);
-        const message = `Welcome to the server, ${member}!`;
-        await channel.send(message);
-    }
+//         await member.roles.add(role);
+//         const message = `Welcome to the server, ${member}!`;
+//         await channel.send(message);
+//     }
 
-    catch (error) {
-        console.error(error);
-    }
-});
+//     catch (error) {
+//         console.error(error);
+//     }
+// });
 
 client.on(Events.MessageCreate, async (message) => {
     if (message.content === '!ping') {
@@ -46,6 +55,40 @@ client.on(Events.MessageCreate, async (message) => {
         try {
             const guild = message.guild;
             await message.channel.send('Vivek is gay!');
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else if (message.content === '!heil') {
+        try {
+            await message.channel.send('Heil Hmmmmm!');
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else if (message.content === '!kshit') {
+        try {
+            await message.channel.send('when is the 5th coming!');
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else if (message.content === '!chuck') {
+        try {
+            message = await chuck();
+            await message.channel.send(message);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else if (message.content === '!generate') {
+        try {
+            message = await generate();
+            await message.channel.send(message);
         }
         catch (error) {
             console.error(error);
